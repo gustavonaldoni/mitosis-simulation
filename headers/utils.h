ed3aa0b51ee120c16360b1372c0249dfbe4a9959
Gustavo <gunaldoni@gmail.com>
2022-09-04 11:56:23 -0300

    Add the function to manipulate probability and add prototypes

--- headers/utils.h
+++ headers/utils.h
@@ -1,4 +1,17 @@
+int GenerateRandomNumber(int, int);
+bool RandomProbabilityManipulation(int);
+
 int GenerateRandomNumber(int lower, int upper)
 {
     return (rand() % (upper - lower + 1)) + lower;
+}
+
+bool RandomProbabilityManipulation(int percentage)
+{
+    int n;
+    
+    srand(time(NULL));
+    n = (rand() % 100) + 1;
+    
+    return n <= percentage;
 }
