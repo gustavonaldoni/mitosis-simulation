73c3ce16d851959ef0c954a77c4d8936399cfe53
Gustavo <gunaldoni@gmail.com>
2022-09-04 13:34:13 -0300

    Add a way to check if the user interacted with the cells

---
+++ headers/userInteraction.h
@@ -0,0 +1,22 @@
+bool UserClickedOnCell(Cell);
+
+bool UserClickedOnCell(Cell cell)
+{
+    return IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && 
+           CheckCollisionPointCircle(GetMousePosition(), 
+                                   (Vector2){cell.x, cell.y}, 
+                                   cell.radius);
+}
+
+int CheckUserClickOnAllCells(Cell cells[], int maxCells)
+{
+    int i;
+
+    for (i = 0; i < maxCells; i++)
+    {
+        if (UserClickedOnCell(cells[i]))
+            return i;
+    }
+
+    return -1;
+}
