0f2ebe33ac272a3f9d1a0f7cd5c730278cfaef70
Gustavo <gunaldoni@gmail.com>
2022-09-04 10:50:09 -0300

    Remove unnecessary files

--- headers/play.h
+++
@@ -1,8 +0,0 @@
-bool PlayIsValid(Board, int, int);
-
-bool PlayIsValid(Board board, int lineNumber, int columnNumber)
-{
-    return board.canvas[lineNumber][columnNumber] == 0 &&
-           lineNumber >= 0 && lineNumber < 3 &&
-           columnNumber >= 0 && columnNumber < 3;
-}
